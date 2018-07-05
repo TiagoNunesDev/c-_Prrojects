decd076a92aa9aa30f487f24815fc6cd16e8577e
tiago nunes <tiagonunesdev@gmail.com>
2018-07-05 13:13:21 +0100

    teste hello world

---
+++ Test/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 05/07/2018
+ *      Author: tnunes
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	std::cout << "Hello world";
+
+}
+
+
+
+
